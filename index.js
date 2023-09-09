8ee06087b3ab28bdae221c5479c77d9da5352840
seknd <76913607+seknd@users.noreply.github.com>
2023-09-09 17:09:27 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let numbers = ""
+let stringNumbers = ''
+
+
+function calculate(element) {
+    numbers += element
+    console.log(numbers);
+    
+    document.querySelector('.js-calculaton').innerHTML = numbers
+
+}
+
+function cal() {
+   let calculated = eval(numbers)
+    console.log(calculated);
+
+        reset()
+
+    document.querySelector('.js-calculaton').innerHTML = calculated
+
+}
+
+function reset() {
+    numbers = ''
+    console.log(numbers);
+    document.querySelector('.js-calculaton').innerHTML = `Result`
+}
+
